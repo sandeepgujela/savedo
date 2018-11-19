2c468da7f0f971cb3e1aad731c2077767abc8079
Sandeep <sandeep_gujela@yahoo.com>
2018-11-19 08:14:30 +0100

    Updates

---
+++ precache-manifest.0bc287a835447b3825c13a12fab606b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "994d80d5fb005698c49b",
+    "url": "/savedo/static/js/runtime~main.994d80d5.js"
+  },
+  {
+    "revision": "295be52897889c88a2c7",
+    "url": "/savedo/static/js/main.295be528.chunk.js"
+  },
+  {
+    "revision": "76c278588db0f299c692",
+    "url": "/savedo/static/js/1.76c27858.chunk.js"
+  },
+  {
+    "revision": "295be52897889c88a2c7",
+    "url": "/savedo/static/css/main.9f97356b.chunk.css"
+  },
+  {
+    "revision": "d13dc47d9039476bd2fe53eb86cb10b9",
+    "url": "/savedo/index.html"
+  }
+];
